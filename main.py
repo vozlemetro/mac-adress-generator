40d40074ebe1a70d098c13f78ed5b7ee88808597
vozlemetro <tool00032123@gmail.com>
2026-03-19 19:33:02 +0300

    Add a multi counter to create some mac-adresses

--- main.py
+++ main.py
@@ -7,15 +7,21 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.post("/random_macadress/")
-async def random_macadress():
-    mac_list = []
+@app.post("/random_macadress/{counter}")
+async def random_macadress(counter):
+    amount = int(counter)
+    
+    mac_pieces = []
+    mac_finals = []
     hex_char = '0123456789ABCDEF'
-    for x in range(6):
-        r1 = random.choice(hex_char)   
-        r2 = random.choice(hex_char)
-        r3 = r1+r2
-        mac_list.append(r3)
-    final_mac = '-'.join(mac_list)
-    return(final_mac)
+    for y in range(amount):
+        mac_list = []
+        for x in range(6):
+            r1 = random.choice(hex_char)   
+            r2 = random.choice(hex_char)
+            r3 = r1+r2
+            mac_list.append(r3)
+        mac_pieces = '-'.join(mac_list)
+        mac_finals.append(mac_pieces)
+    return(mac_finals)
     
